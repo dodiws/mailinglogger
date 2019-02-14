8d63142a7707aa6a15ca6c85949bf8486b5fd3f6
dodiws <dodiws@gmail.com>
2019-02-14 15:14:57 +0700

    add test docstring

--- mailinglogger/MailingLogger.py
+++ mailinglogger/MailingLogger.py
@@ -15,7 +15,14 @@ flood_template = open(os.path.join(this_dir, 'flood_template.txt')).read()
 
 
 class MailingLogger(SMTPHandler):
+    """
+    This is a reST style.
 
+    :param param1: this is a first param
+    :param param2: this is a second param
+    :returns: this is a description of what is returned
+    :raises keyError: raises an exception
+    """
     now = datetime.datetime.now
 
     def __init__(self,
